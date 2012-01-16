95dcac5f9ec2916d7e7ad84086939cc28511a891
rene.de.bloois@gmail.com <rene.de.bloois@gmail.com@f0fa30ad-b03f-d517-0e59-13788262ff8e>
2012-01-16 20:53:44 +0000

    Made JPASupport.listOfEntities() static and renamed to getResultList() to better match with JPA. Added JPASupport.singleEntity(). Added JPASupport.createQuery(). Added unit tests that experiments with generics.

--- trunk/src/solidstack/query/JPASupport.java
+++ trunk/src/solidstack/query/JPASupport.java
@@ -23,12 +23,42 @@ public class JPASupport
 	 * @param entityClass The class to map the results to.
 	 * @return A {@link List} of entities.
 	 */
-	public List<?> listOfEntities(Query query, EntityManager entityManager, Class<?> entityClass) {
+	@SuppressWarnings( "unchecked" )
+	static public <T> List< T > getResultList( Query query, EntityManager entityManager, Class< T > entityClass )
+	{
+		javax.persistence.Query jpaQuery = createQuery( query, entityManager, entityClass );
+		return jpaQuery.getResultList();
+	}
 
+	/**
+	 * Retrieves a single JPA Entity from the given {@link EntityManager}.
+	 * 
+	 * @param query The query.
+	 * @param entityManager The {@link EntityManager} to use.
+	 * @param entityClass The class to map the results to.
+	 * @return An entity.
+	 */
+	@SuppressWarnings( "unchecked" )
+	static public <T> T getSingleResult( Query query, EntityManager entityManager, Class< T > entityClass )
+	{
+		javax.persistence.Query jpaQuery = createQuery( query, entityManager, entityClass );
+		return (T)jpaQuery.getSingleResult();
+	}
+
+	/**
+	 * Creates a JPA query.
+	 * 
+	 * @param query The query.
+	 * @param entityManager The {@link EntityManager} to use.
+	 * @param entityClass The class to map the results to.
+	 * @return The JPA query.
+	 */
+	static public javax.persistence.Query createQuery( Query query, EntityManager entityManager, Class< ? > entityClass )
+	{
 		List< Object > pars = new ArrayList< Object >();
 		String preparedSql = query.getPreparedSQL( pars );
 
-		javax.persistence.Query jpaQuery = entityManager.createNativeQuery( preparedSql, entityClass );
+		javax.persistence.Query result = entityManager.createNativeQuery( preparedSql, entityClass );
 		int i = 0;
 		for( Object par : pars )
 		{
@@ -37,9 +67,8 @@ public class JPASupport
 				Assert.isFalse( par instanceof Collection );
 				Assert.isFalse( par.getClass().isArray() );
 			}
-			jpaQuery.setParameter( ++i, par );
+			result.setParameter( ++i, par );
 		}
-
-		return jpaQuery.getResultList();
+		return result;
 	}
 }
