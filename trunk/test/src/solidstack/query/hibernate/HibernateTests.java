0d2a3346b30187bdd3e5d07c2abb797d397dc8a9
rene.de.bloois@gmail.com <rene.de.bloois@gmail.com@f0fa30ad-b03f-d517-0e59-13788262ff8e>
2012-02-14 21:01:19 +0000

    Version 4.1.0 of Hibernate and version 2.0 of JPA.

--- trunk/test/src/solidstack/query/hibernate/HibernateTests.java
+++ trunk/test/src/solidstack/query/hibernate/HibernateTests.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
@@ -16,7 +17,7 @@ import solidstack.util.Pars;
 
 public class HibernateTests
 {
-	public SessionFactory factory;
+	private SessionFactory factory;
 
 	@BeforeClass(groups="new")
 	public void init()
@@ -24,26 +25,36 @@ public class HibernateTests
 		this.factory = new Configuration().configure().buildSessionFactory();
 	}
 
-	@Test
+	@Test(groups="new")
 	public void testCriteria()
 	{
 		Session session = this.factory.openSession();
+
 		List< DerbyTable > tables = session.createCriteria( DerbyTable.class ).list();
 		for( DerbyTable table : tables )
 			System.out.println( table.getName() );
+
 		session.close();
 	}
 
-	@Test
+	@Test(groups="new")
 	public void testListOfMaps()
 	{
+		Session session = this.factory.openSession();
+
 		QueryManager queries = new QueryManager();
 		queries.setPackage( "solidstack.query" );
 		Query query = queries.getQuery( "test" );
-		Session session = this.factory.openSession();
 		List<Map<String, Object>> tables = query.hibernate().listOfMaps( session, new Pars() );
 		for( Map<String, Object> table : tables )
 			System.out.println( table.get( "TaBlEnAmE" ) );
+
 		session.close();
+	}
+
+	@AfterClass(groups="new")
+	public void exit()
+	{
+		//this.factory.close(); TODO Why does this give java.sql.SQLException: Cannot close a connection while a transaction is still active?
 	}
 }
