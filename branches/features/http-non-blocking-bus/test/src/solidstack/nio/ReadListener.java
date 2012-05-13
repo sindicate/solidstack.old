ef77443ba46cfd8b1eb5910c4a0c7fa8cee844af
rene.de.bloois@gmail.com <rene.de.bloois@gmail.com@f0fa30ad-b03f-d517-0e59-13788262ff8e>
2012-05-13 12:01:26 +0000

    Removed non blocking NIO sources.

--- branches/features/http-non-blocking-bus/test/src/solidstack/nio/ReadListener.java
+++
@@ -1,9 +0,0 @@
-package solidstack.nio;
-
-import java.io.IOException;
-
-public interface ReadListener
-{
-	void incoming( AsyncSocketChannelHandler handler ) throws IOException;
-	void timeout() throws IOException;
-}
