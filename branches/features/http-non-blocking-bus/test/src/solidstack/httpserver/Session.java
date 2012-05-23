20a4319303f4a502e330221fc94aa2ff8c79bc9f
rene.de.bloois@gmail.com <rene.de.bloois@gmail.com@f0fa30ad-b03f-d517-0e59-13788262ff8e>
2012-05-23 19:29:03 +0000

    Added MySQL. Oracle may be broken now.

--- branches/features/http-non-blocking-bus/test/src/solidstack/httpserver/Session.java
+++ branches/features/http-non-blocking-bus/test/src/solidstack/httpserver/Session.java
@@ -3,17 +3,21 @@ package solidstack.httpserver;
 import java.util.HashMap;
 import java.util.Map;
 
+import solidstack.nio.DebugId;
+
 public class Session
 {
 	private Map<String, Object> attributes = new HashMap<String, Object>();
 
 	public void setAttribute( String name, Object value )
 	{
+		Loggers.httpServer.debug( "setAttribute, session: {}, {}", DebugId.getId( this ), name );
 		this.attributes.put( name, value );
 	}
 
 	public Object getAttribute( String name )
 	{
+		Loggers.httpServer.debug( "getAttribute, session: {}, {}", DebugId.getId( this ), name );
 		return this.attributes.get( name );
 	}
 }
