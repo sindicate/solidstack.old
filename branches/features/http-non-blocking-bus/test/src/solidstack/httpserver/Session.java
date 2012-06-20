237c8503dee732ba0d2fd7441039fc3b94dbe06b
rene.de.bloois@gmail.com <rene.de.bloois@gmail.com@f0fa30ad-b03f-d517-0e59-13788262ff8e>
2012-06-20 11:04:33 +0000

    Fixed compile error.

--- branches/features/http-non-blocking-bus/test/src/solidstack/httpserver/Session.java
+++ branches/features/http-non-blocking-bus/test/src/solidstack/httpserver/Session.java
@@ -3,21 +3,19 @@ package solidstack.httpserver;
 import java.util.HashMap;
 import java.util.Map;
 
-import solidstack.nio.DebugId;
-
 public class Session
 {
 	private Map<String, Object> attributes = new HashMap<String, Object>();
 
 	public void setAttribute( String name, Object value )
 	{
-		Loggers.httpServer.debug( "setAttribute, session: {}, {}", DebugId.getId( this ), name );
+//		Loggers.httpServer.debug( "setAttribute, session: {}, {}", DebugId.getId( this ), name );
 		this.attributes.put( name, value );
 	}
 
 	public Object getAttribute( String name )
 	{
-		Loggers.httpServer.debug( "getAttribute, session: {}, {}", DebugId.getId( this ), name );
+//		Loggers.httpServer.debug( "getAttribute, session: {}, {}", DebugId.getId( this ), name );
 		return this.attributes.get( name );
 	}
 }
