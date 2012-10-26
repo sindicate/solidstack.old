a0a2b3f2676c01d9614c0f40809947f6bacd37ce
rene.de.bloois@gmail.com <rene.de.bloois@gmail.com@f0fa30ad-b03f-d517-0e59-13788262ff8e>
2012-10-26 21:55:53 +0000

    Added copyright headers.

--- branches/features/scripting2/src/solidstack/script/ObjectAccess.java
+++ branches/features/scripting2/src/solidstack/script/ObjectAccess.java
@@ -1,6 +1,7 @@
 /*--
- * Copyright 2012 René import solidstack.script.java.Java;
-e Apache License, Version 2.0 (the "License");
+ * Copyright 2012 René M. de Bloois
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
