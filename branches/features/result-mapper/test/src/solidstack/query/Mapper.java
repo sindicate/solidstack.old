55a56212cd72ea8b04af164422cb8ca9088dbd79
rene.de.bloois@gmail.com <rene.de.bloois@gmail.com@f0fa30ad-b03f-d517-0e59-13788262ff8e>
2013-02-21 21:01:40 +0000

    Added mapper test (not working).

---
+++ branches/features/result-mapper/test/src/solidstack/query/Mapper.java
@@ -0,0 +1,47 @@
+/*--
+ * Copyright 2006 René M. de Bloois
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package solidstack.query;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
+
+import org.testng.annotations.Test;
+
+import solidstack.util.Pars;
+
+
+@SuppressWarnings( "javadoc" )
+public class Mapper
+{
+	@Test
+	public void testBasic() throws ClassNotFoundException, SQLException
+	{
+		Class.forName( "org.apache.derby.jdbc.EmbeddedDriver" );
+		Connection connection = DriverManager.getConnection( "jdbc:derby:memory:test;create=true", "app", null );
+
+		QueryLoader queries = new QueryLoader();
+		queries.setTemplatePath( "classpath:/solidstack/query" );
+
+		Query query = queries.getQuery( "mapper.sql" );
+
+		List< Map< String, Object > > result = query.listOfMaps( connection, Pars.EMPTY );
+		assert result.size() == 22;
+	}
+}
